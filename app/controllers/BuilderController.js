a25c7596ef32edd7268a712165df5847156f6029
Imensha <zhyef37@gmail.com>
2014-11-12 17:51:04 +0100

    Deck card enter/leave animations added

--- app/controllers/BuilderController.js
+++ app/controllers/BuilderController.js
@@ -1,17 +1,16 @@
-function BuilderController(Cards, Elements) {
+function BuilderController($animate, Cards, Elements) {
 	this.elements = Elements;
 	this.allCards = Cards;
 	this.currentDeck = [];
 	this.filterElement = null;
 
 	this.deckAdd = function(card) {
-		this.currentDeck.push(card);
+		this.currentDeck.push(angular.copy(card));
 	}
 	this.deckRemove = function(i) {
 		this.currentDeck.splice(i, 1);
 	}
 	this.hasImage = function(card) {
-			console.log(card);
 		if(typeof card.image === "undefined" || card.image.search(/^data/) > -1) {
 			return false;
 		} else {
