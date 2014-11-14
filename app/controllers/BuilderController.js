6a27e3bf48dc8b5fd6cb85733e5a723b644823d7
Imensha <zhyef37@gmail.com>
2014-11-14 01:12:32 +0100

    Added pagination on card overview. Closes #8

--- app/controllers/BuilderController.js
+++ app/controllers/BuilderController.js
@@ -1,22 +1,49 @@
-function BuilderController($animate, Cards, Elements) {
+function BuilderController($animate, $filter, Cards, Elements) {
 	this.elements = Elements;
 	this.elements.unshift({name: "All", value: ""});
 	this.allCards = Cards;
 	this.currentDeck = [];
 	this.filterElement = this.elements[0].value;
-	this.cardSearch = "";
+	this.searchText = "";
 
 	this.deckAdd = function(card) {
 		this.currentDeck.push(angular.copy(card));
-	}
+	};
 	this.deckRemove = function(i) {
 		this.currentDeck.splice(i, 1);
-	}
+	};
 	this.hasImage = function(card) {
 		if(typeof card.image === "undefined" || card.image.search(/^data/) > -1) {
 			return false;
 		} else {
 			return true;
 		}
+	};
+
+	this.currentPage = 1;
+	this.itemsPerPage = 12;
+	this.maxPages = 5;
+	this.filteredCards = $filter("filter")(this.allCards, {element: this.filterElement});
+	this.pageCards = this.filteredCards.slice(0, this.itemsPerPage);
+	this.filterCards = function() {
+		var filterByElement = $filter("filter")(this.allCards, {element: this.filterElement});
+		var filterBySearch = $filter("filter")(filterByElement, this.searchText);
+		this.filteredCards = filterBySearch;
+	};
+	this.filteredCardCount = function() {
+		return this.filteredCards.length;
 	}
+	this.elementChange = function() {
+		this.currentPage = 1;
+		this.filterCards();
+		this.pageChange();
+	};
+	this.searchChange = function() {
+		this.currentPage = 1;
+		this.filterCards();
+		this.pageChange();
+	};
+	this.pageChange = function() {
+		this.pageCards = this.filteredCards.slice((this.currentPage-1)*this.itemsPerPage, this.currentPage*this.itemsPerPage);
+	};
 }
